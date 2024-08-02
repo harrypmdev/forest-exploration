fb27bdc0452b5066cda7840c0f09777e0f1cd849
Harry Miles <harrypetermiles@gmail.com>
2024-08-02 13:32:41 +0000

    feat: add parse_describe_move function to utility.py

--- run.py
+++ run.py
@@ -7,52 +7,9 @@ from effect import Effect
 from item import *
 from utility import *
 
-def parse_move(move, board, player):
-    board_moves = ("N", "E," "S", "S", "map")
-    if move == "help":
-        print("\nValid moves:\n"
-        "inventory - prints your current inventory\n"
-        "status - prints your current status\n"
-        "map - prints the map\n"
-        "use (item name) - uses an item\n"
-        "use (item name) on (target) - uses an item on a target\n"
-        "N - moves North\n"
-        "E - moves East\n"
-        "S - moves South\n"
-        "W - moves West\n"
-        )
-    elif move in ("status", "health", "points", "score"):
-        player.print_status()
-    elif move == "inventory":
-        player.print_inventory()
-    elif move == "break":
-        exit()
-    elif "use" in move:
-        return parse_use_move(move, board, player)
-    elif move in board_moves:
-        board.parse_move(move, player)
-
-def parse_use_move(move, board, player):
-    parts = move.split()
-    if parts[0] != "use" or parts[1] not in (item.name for item in player.inventory):
-        return False
-    if len(parts) == 2:
-        for item in player.inventory:
-            if item.name == parts[1]:
-                item.use(player)
-    elif len(parts) == 4:
-        if parts[2] != "on":
-            return False
-        for entity in board.entities:
-            if parts[4] == entity.name:
-                player.inventory.index(parts[1].use(entity))
-    else:
-        return false
-
 def get_move():
     print("Enter 'help' for instructions.")
-    move = input("Enter a move: ")
-    return move
+    return input("Enter a move: ")
 
 def main():
     board = GameBoard(5)
@@ -61,11 +18,6 @@ def main():
     player.inventory.append(potion)
     board.introduce(player)
     player.print_status()
-    ogre = Enemy(20, "ogre", board, 5, 0.7, "was clubbed")
-    ogre.print_status()
-    ogre.attack(player)
-    ogre.attack(player)
-    ogre.attack(player)
     while player.alive:
         move = get_move()
         parse_move(move, board, player)
--- utility.py
+++ utility.py
@@ -8,3 +8,60 @@ def get_emojis(*args):
     for arg in args:
         emoji_list.append(emoji.emojize(arg))
     return emoji_list
+
+def parse_move(move, board, player):
+    board_moves = ("N", "E," "S", "S", "map")
+    if move == "help":
+        print("\nValid moves:\n"
+        "inventory - prints your current inventory\n"
+        "status - prints your current status\n"
+        "map - prints the map\n"
+        "describe (item name) - gives the description of an inventory item\n"
+        "use (item name) - uses an item\n"
+        "use (item name) on (target) - uses an item on a target\n"
+        "N - moves North\n"
+        "E - moves East\n"
+        "S - moves South\n"
+        "W - moves West\n"
+        )
+    elif move in ("status", "health", "points", "score"):
+        player.print_status()
+    elif move == "inventory":
+        player.print_inventory()
+    elif move == "break":
+        exit()
+    elif "use" in move:
+        return parse_use_move(move, board, player)
+    elif "describe" in move:
+        return parse_describe_move(move, player)
+    elif move in board_moves:
+        board.parse_move(move, player)
+
+def parse_use_move(move, board, player):
+    parts = move.split()
+    if parts[0] != "use" or parts[1] not in (item.name for item in player.inventory):
+        return False
+    if len(parts) == 2:
+        for item in player.inventory:
+            if item.name == parts[1]:
+                item.use(player)
+    elif len(parts) == 4:
+        if parts[2] != "on":
+            return False
+        for entity in board.entities:
+            if parts[4] == entity.name:
+                player.inventory.index(parts[1].use(entity))
+    else:
+        return false
+
+def parse_describe_move(move, player):
+    parts = []
+    parts.append(move[:8])
+    parts.append(move[8:])
+    parts = [part.strip() for part in parts]
+    if parts[0] != "describe" or parts[1] not in (item.name for item in player.inventory):
+        return False
+    for item in player.inventory:
+        if item.name == parts[1]:
+            print("\n" + item.description + "\n")
+            return True
