d0ca559ab0383faeff903add0d7737978b4d99ad
Harry Miles <harrypetermiles@gmail.com>
2024-08-06 15:34:44 +0000

    fix: optimised, annotated and commented game_board.py

--- game_board.py
+++ game_board.py
@@ -1,8 +1,13 @@
+"""
+A module for the GameBoard class utilised in Forest Exploration.
+"""
+
 import math
 import random
 from utility import *
 from area import Area
 from enemy import Enemy
+from player import Player
 from item import *
 
 class GameBoard:
@@ -10,7 +15,9 @@ class GameBoard:
     A class for the game board.
 
     Class Attributes:
-    DIRECTIONS: tuple -- A tuple containing the directions the player can move
+    DIRECTIONS: dict -- A dictionary containing the directions the player
+    can move, the axis it moves them on (0 for y, 1 for x) and the direction
+    on the axis it moves them.
 
     Instance Attributes:
     size: int -- the dimension size of the board
@@ -30,9 +37,14 @@ class GameBoard:
     end_turn -- end the turn
     flee -- attempt to 'flee' the player
     """
-    DIRECTIONS = ("north", "east", "south", "west")
+    DIRECTIONS = {
+        "north": (0, -1), 
+        "east": (1, 1), 
+        "south": (0, 1), 
+        "west": (1, -1)
+    }
     
-    def __init__(self, size):
+    def __init__(self, size) -> None:
         """
         Create a GameBoard object.
     
@@ -68,8 +80,14 @@ class GameBoard:
         items[Amulet("amulet", "Could it be... the amulet of power? There's only one way to find out.")] = 0
         return items
 
-    def _add_to_visited(self, location: Area):
-        """ Add an area to visited locations and update map. """
+    def _add_to_visited(self, location: Area) -> None:
+        """ 
+        Check if a location is already on the visited list and add it if not.
+        Update map for the visited area.
+        
+        Arguments:
+        location: Area -- the new area that needs to be on the visited list
+        """
         if not self._check_visited(location.y, location.x):
             self.visited.append(location)
         if self.currently_in_battle():
@@ -77,36 +95,65 @@ class GameBoard:
         else:
             self.map[location.y][location.x] = get_emojis(":radio_button:")[0]   
 
-    def _update_amulet_generation_probability(self):
-        """ C """
+    def _update_amulet_generation_probability(self) -> None:
+        """ Update probability of amulet generating based on amount of areas visited. """
         for item in self.item_field:
             if item.name == "amulet":
                 self.item_field[item] = int(not self.amulet_generated) * (1 / (self.size*self.size))  * len(self.visited)
     
-    def _check_visited(self, y, x):
+    def _check_visited(self, y: int, x: int) -> bool:
+        """ 
+        Check if the player has visited an area at the given coordinates.
+
+        Arguments:
+        y: int -- the y coordinate to check
+        x: int -- the x coordinate to check
+
+        Returns True if it has been visited, False if not.
+        """
         for area in self.visited:
             if area.y == y and area.x == x:
                 return True
         return False
     
-    def _get_visited_area(self, y, x):
+    def _get_visited_area(self, y: int, x: int) -> bool:
+        """ 
+        Return the area object for the given coordinates.
+        Coordinates for a valid area that has already been visited
+        must be provided.
+
+        Arguments:
+        y: int -- the y coordinate to retrieve
+        x: int -- the x coordinate to retrieve
+
+        Raises ValueError if passed invalid coordinates.
+
+        Returns Area object for the coordinates in question.
+        """
         for area in self.visited:
             if area.y == y and area.x == x:
                 return area
+        raise ValueError(f"Inappropriate coordinates provided: no area in visited list for coordinates y:{y}, x:{x}.")
 
-    def currently_in_battle(self):
+    def currently_in_battle(self) -> bool:
+        """ 
+        Check if the player is currently in battle (hostile entities are present).
+        Returns True if in battle, False if not.
+        """
         for entity in self.current_location.entities:
             if type(entity) == Enemy and entity.alive:
                 return True
         return False
     
-    def print(self):
+    def print(self) -> None:
+        """ Print the game board's map. """
         print("")
         for row in self.map:
             print(" ".join(row))
         print("")
     
-    def introduce(self, player):
+    def introduce(self, player: Player) -> None:
+        """ Print a game introductory message. """
         print("═══━━━━━━━━━────────────────── • ──────────────────━━━━━━━━━═══\n"
               "Welcome to Forest Exploration!\n"
               f"Your game board has {self.size}x{self.size} dimensions.\n"
@@ -114,58 +161,69 @@ class GameBoard:
               "Enter 'tutorial' if it is your first time playing.\n"
               "═══━━━━━━━━━────────────────── • ──────────────────━━━━━━━━━═══")
 
-    def move(self, direction, fleeing = False):
-        if direction not in self.DIRECTIONS:
-            raise GameError("\nThe 'go' command must be followed by: North, East, South or West.\n")
-        if self.in_battle and not fleeing:
-            print("\nCannot travel whilst in battle! Enter 'flee' to attempt to flee.\n")
-            return False
-        new_x = self.current_location.x
-        new_y = self.current_location.y
-        if direction == "north":
-            new_y = self.current_location.y - 1
-        elif direction == "south":
-            new_y = self.current_location.y + 1
-        elif direction == "east":
-            new_x = self.current_location.x + 1
-        elif direction == "west":
-            new_x = self.current_location.x - 1
-        travel_string = f"\nYou travelled {direction.capitalize()}"
-        if 4 >= new_x >= 0 and 4 >= new_y >= 0:
-            self._update_amulet_generation_probability()
-            self.records["total moves"] += 1
-            if fleeing:
-                print("Fled successfully!")
-            print(travel_string)
-            self._add_to_visited(self.current_location)
-            if self._check_visited(new_y, new_x):
-                self.current_location = self._get_visited_area(new_y, new_x)
-            else:
-                self.current_location = Area(new_y, new_x, self)
-            self.map[self.current_location.y][self.current_location.x] = get_emojis(":diamond_with_a_dot:")[0]
-            self.look()
-        else:
-            raise GameError("\nCannot move in this direction.\n")
+    def move(self, direction: str, fleeing: bool = False) -> bool:
+        """
+        Move in a specified direction on the game board's map.
 
-    def look(self, line_break = True):
+        Arguments:
+        direction: str -- the direction the player should move (north, east, south or west)
+        fleeing: bool -- whether the player is currently fleeing (default False)
+
+        Raises GameError if:
+            The provided direction is invalid
+            An attempt to move while in battle is made but the player is not fleeing
+        
+        Returns False
+        """
+        if direction not in self.DIRECTIONS.keys():
+            raise GameError("\nThe 'go' command must be followed by: North, East, South or West.\n")
+        if self.currently_in_battle() and not fleeing:
+            raise GameError("\nCannot travel whilst in battle! Enter 'flee' to attempt to flee.\n")
+        new_direction = (self.current_location.y, self.current_location.x)
+        new_direction[self.DIRECTIONS[direction][0]] += self.DIRECTIONS[direction][1]
+        if not (4 >= new_direction[0] >= 0 and 4 >= new_direction[1] >= 0):
+            raise GameError("\nCannot move in this direction.\n")
+        self.records["total moves"] += 1
+        self._add_to_visited(self.current_location)
+        self._update_amulet_generation_probability()
+        if fleeing:
+            print("Fled successfully!")
+        print(f"\nYou travelled {direction.capitalize()}")
+        if self._check_visited(*new_direction):
+            self.current_location = self._get_visited_area(*new_direction)
+        else:
+            self.current_location = Area(*new_direction, self)
+        self.map[self.current_location.y][self.current_location.x] = get_emojis(":diamond_with_a_dot:")[0]
+        self.look()
+        return False
+            
+
+    def look(self, line_break: bool = True) -> None:
+        """ Print a description of the current area. """
         line_break = "\n" if line_break else ""
         print(f"{line_break}{self.current_location.get_description()}")
         if self.currently_in_battle():
             print("A hostile creature is present! You are in battle.\n")
     
-    def end_turn(self, player):
-        self.in_battle = self.currently_in_battle()
+    def end_turn(self, player: Player) -> None:
+        """ Ends the turn, making all living enemies attack the player. """
         for entity in self.current_location.entities:
             if type(entity) == Enemy and entity.alive and player.alive:
                 entity.attack(player)
     
-    def flee(self):
-        if not self.in_battle:
-            print("\nCannot flee if not in battle! Move using 'go' command.\n")
-            return False
+    def flee(self) -> bool:
+        """ 
+        Attempt to 'flee' the player.
+
+        Raises GameError is player is not in battle.
+
+        Returns True.
+        """
+        if not self.currently_in_battle:
+            raise GameError("\nCannot flee if not in battle! Move using 'go' command.\n")
         if random.random() > 0.3:
             try:
-                self.move(random.choice(self.DIRECTIONS), True)
+                self.move(random.choice(self.DIRECTIONS.keys()), True)
             except GameError:
                 self.flee()
         else:
